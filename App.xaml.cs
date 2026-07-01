0020e77628d166f4179c5c7973bd1423eca7fc44
miki <mikiboii21@gmail.com>
2026-07-01 20:51:20 +0300

    cleaning up files

--- App.xaml.cs
+++ App.xaml.cs
@@ -31,18 +31,18 @@ public partial class App : System.Windows.Application
       
        
         
-        var splash = new Splash_screen();
-        splash.Show();
-        // Optionally do some heavy initialization here
-        await Task.Run(() =>
-        {
-            // Simulate work like loading data, initializing services, etc.
-            System.Threading.Thread.Sleep(3000);
-        });
-        
-        // 3️⃣ Close splash and show main window
-        splash.Close();
-        
+        // var splash = new Splash_screen();
+        // splash.Show();
+        // // Optionally do some heavy initialization here
+        // await Task.Run(() =>
+        // {
+        //     // Simulate work like loading data, initializing services, etc.
+        //     System.Threading.Thread.Sleep(3000);
+        // });
+        //
+        // // 3️⃣ Close splash and show main window
+        // splash.Close();
+        //
         
         
         home.Show();
